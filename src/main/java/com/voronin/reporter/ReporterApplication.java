1bd58a195156656bd3cbf70f391b675e16b297fe
Kirill Voronin <yoursdearboy@gmail.com>
2019-07-23 18:11:47 +0300

    Reporter starter

---
+++ src/main/java/com/voronin/reporter/ReporterApplication.java
@@ -0,0 +1,13 @@
+package com.voronin.reporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporterApplication.class, args);
+	}
+
+}
